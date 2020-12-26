9da5c6e715eeaf8817b8abdac07151164db1e6c5
Mamun <mamun511510782@gmail.com>
2020-12-26 09:07:56 +0600

    data type

---
+++ 03datatype.php
@@ -0,0 +1,23 @@
+<?php
+    $x = "Mamun";
+    var_dump($x);
+    echo "<br>";
+    $x = '2500';
+    var_dump($x);
+    echo "<br>";
+    $x = "2550.50";
+    var_dump($x);
+    echo "<br>";
+    $x = false;
+    var_dump($x);
+    echo "<br>";
+    $x = null;
+    var_dump($x);
+    echo "<br>";
+    $x = 2550.5;
+    var_dump($x);
+    echo "<br>";
+    $x = array("html","css","js");
+    echo $x[1]."<br>";
+    var_dump($x);
+?>
