4488d75860604fbc0a0d0194e40e623d4bbb688e
Mamun <mamun511510782@gmail.com>
2020-12-27 09:55:50 +0600

    Comparison Operator

---
+++ 08comparison-operator.php
@@ -0,0 +1,12 @@
+<?php
+    $a = 10;
+    $b = 10;
+
+    echo $a == $b;
+    echo $a === $b; //equal with data type
+    echo $a != $b;
+    echo $a > $b;
+    echo $a < $b;
+    echo $a <= $b;
+    echo $a <=> $b;
+?>
