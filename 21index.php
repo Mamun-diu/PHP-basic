ad7f1476f87a42d923a1d6f1cdce3d9580ff7317
Mamun <mamun511510782@gmail.com>
2021-01-07 18:48:30 +0600

    Goto Statement

---
+++ 21index.php
@@ -0,0 +1,13 @@
+<?php
+    
+    echo "Hello World<br>";
+    echo "Hello World<br>";
+    goto abc;
+    echo "Hello World<br>";
+    echo "Hello World<br>";
+    echo "Hello World<br>";
+    echo "Hello World<br>";
+
+    abc:
+    echo "Hey this is label ABC<br>";
+?>
