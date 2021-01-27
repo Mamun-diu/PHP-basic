960d434f0b534d1d7ca6ab8c8329fd4909c87c2a
Mamun <mamun511510782@gmail.com>
2021-01-27 14:54:04 +0600

    Function

---
+++ 22index.php
@@ -0,0 +1,15 @@
+<?php
+    function hello(){
+        echo "Hello Everybody.<br>";
+    }
+    function name(){
+        echo "My name is Mamun.<br>";
+    }
+
+    hello();
+    hello();
+    name();
+    hello();
+    hello();
+    
+?>
