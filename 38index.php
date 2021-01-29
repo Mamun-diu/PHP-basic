0bd2fe255855462d1855f18ee7ba78da597aaf21
Mamun <mamun511510782@gmail.com>
2021-01-29 19:28:21 +0600

    array_pop and array_push

---
+++ 38index.php
@@ -0,0 +1,11 @@
+<?php
+// array_pop and array_push
+
+    $fruit = ['orange','banana','apple','grapes'];
+
+    array_pop($fruit); //orange,banana,apple
+    array_push($fruit,'mango'); // orange,banana,apple,mango
+    print_r($fruit);
+    
+
+?>
