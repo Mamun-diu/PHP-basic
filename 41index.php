f4c61827b36f7d1103b38599ed604e3a9fffb588
Mamun <mamun511510782@gmail.com>
2021-01-29 20:14:40 +0600

    array_slice function

---
+++ 41index.php
@@ -0,0 +1,13 @@
+<?php
+// array_slice function
+
+$color = ['red', 'green', 'blue', 'yellow', 'brown'];
+
+$newArray = array_slice($color, 1, 2);
+print_r($newArray); //green, blue
+
+
+
+    
+
+?>
