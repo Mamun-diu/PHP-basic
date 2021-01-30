110f225adfa8618399f29628fa2de61abd23909c
Mamun <mamun511510782@gmail.com>
2021-01-30 07:47:56 +0600

    array Traversing

---
+++ 47index.php
@@ -0,0 +1,23 @@
+<?php
+// array Traversing
+
+$food = ['orange', 'banana', 'apple', 'grapes'];
+echo "<b>Current : </b>". current($food)."<br>";
+echo "<b>Key : </b>". key($food) ."<br>";
+echo "<b>Pos : </b>". pos($food)."<br>";
+
+next($food);
+echo "<b>Current : </b>". current($food)."<br>";
+
+next($food);
+echo "<b>Current : </b>". current($food)."<br>";
+
+prev($food);
+echo "<b>Current : </b>". current($food)."<br>";
+
+end($food);
+echo "<b>Current : </b>". current($food)."<br>";
+
+
+
+?>
